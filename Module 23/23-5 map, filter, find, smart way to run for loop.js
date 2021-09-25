3f3e111b71091843353aa899742b577f15e2b3d7
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 23:49:41 +0600

    Mapping

--- Module 23/23-5 map, filter, find, smart way to run for loop.js
+++ Module 23/23-5 map, filter, find, smart way to run for loop.js
@@ -6,3 +6,11 @@ for (let i = 0; i < numbers.length; i++) {
     output.push(element);
 }
 console.log(output);
+
+
+// const secondResult =numbers.map(function(element) {
+//     return element * 2;
+// })
+const secondResult =numbers.map(element => element * 2);
+
+console.log(secondResult)

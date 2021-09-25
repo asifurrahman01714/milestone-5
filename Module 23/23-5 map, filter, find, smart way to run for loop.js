d89a1fdd334d1e49416ca3ea4f8ecc948b96b0bb
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 23:43:38 +0600

    inserting value in an empty array

--- Module 23/23-5 map, filter, find, smart way to run for loop.js
+++ Module 23/23-5 map, filter, find, smart way to run for loop.js
@@ -1,5 +1,8 @@
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12];
+const output = [];
 for (let i = 0; i < numbers.length; i++) {
     const element = numbers[i] * 2;
     console.log(element);
+    output.push(element);
 }
+console.log(output);

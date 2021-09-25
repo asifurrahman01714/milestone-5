83e2a66e85a7ff23dd869122adaebf00f87f41cc
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 12:20:42 +0600

    everything change is possible in a array const variable. But you can't change the overall array

--- Module 22/22-2 Let, const, array declared with const, object declared with const.js
+++ Module 22/22-2 Let, const, array declared with const, object declared with const.js
@@ -2,3 +2,12 @@
 const name = 'Asif Ur Rahman';
 console.log(name);
 // name = 'sakib ur rahman'-- It is not possible
+
+// const with array variable
+const studentID = [10,13]
+studentID.push(12)
+console.log(studentID)
+studentID[1]=45
+console.log(studentID);
+// everything change is possible in a array const variable. But you can't change the overall array
+// studentID=['asif','sakib']

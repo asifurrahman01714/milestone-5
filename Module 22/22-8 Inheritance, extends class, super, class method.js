e207be43385247e88b236aa6ccf6061b92618984
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 15:26:38 +0600

    Class inheritance, super

--- Module 22/22-8 Inheritance, extends class, super, class method.js
+++ Module 22/22-8 Inheritance, extends class, super, class method.js
@@ -1,8 +1,27 @@
-class Student{
-    constructor(name){
-        this.name = name;
+// class Parent{
+//     constructor(parent){
+//         this.parentName = parent;
+//     }
+// }
+class Parent{
+    constructor(){
+        this.parentName = 'Motiur Rahman';
     }
 }
 
+
+class Student extends Parent{
+    // constructor(parent,name){
+    constructor(name){
+        super();
+        this.name = name;
+    }
+    getFullName(){
+        return this.name + " " + this.parentName;
+    }
+}
+
+// const student = new Student("Motiur Rahman","Asif");
 const student = new Student("Asif");
-console.log(student)
+console.log(student);
+console.log(student.getFullName())

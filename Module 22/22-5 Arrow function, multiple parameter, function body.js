803f1d4fbf78d09572996f455110a43365ca416d
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 14:14:17 +0600

    Single line arrow function

--- Module 22/22-5 Arrow function, multiple parameter, function body.js
+++ Module 22/22-5 Arrow function, multiple parameter, function body.js
@@ -16,3 +16,12 @@ const doubleIt3 = function asif(x) {
 }
 console.log(doubleIt3(4));
 
+// Function with ES6 and arrow functions
+
+// Single line arrow function
+const addNumbers1 = (x, y) => x + y;
+console.log(addNumbers1(2,3));
+
+const doubleIt4 = x => 3*2;
+console.log(doubleIt4(3));
+

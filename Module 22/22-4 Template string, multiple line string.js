f339195e0c5cf7dcaba2557241876c9902599647
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 12:50:00 +0600

    ES6 templete string

---
+++ Module 22/22-4 Template string, multiple line string.js
@@ -0,0 +1,15 @@
+const firstName ='Asif Ur'
+const lastName ='Rahman'
+const fullName1 = firstName + ' '+ lastName + ' is a good boy'
+console.log(fullName1);
+
+const fullNameWithES6 = `${firstName} ${lastName} is a good boy . He achieved marks ${10+20}`;
+console.log(fullNameWithES6);
+
+const multiLineWithES15 = "I Love u Allah\n" + "I need you"
+console.log(multiLineWithES15)
+
+const multiLineWithES6 = ` 
+I love you Allah
+I need you`;
+console.log(multiLineWithES6)

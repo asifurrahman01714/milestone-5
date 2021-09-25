4265d02c1778e02f09b1c7f983aa20a3d41d9cfb
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 15:52:04 +0600

    Object Distructuring

---
+++ Module 22/22-9 Destructure, Object, array, destructure complex object.js
@@ -0,0 +1,14 @@
+const student ={
+    id : 12,
+    info : {
+        address : "Chittagong"
+    },
+    phone : 121212
+}
+
+const {id,phone} =student;
+console.log(id,phone);
+
+
+const {address} = student.info;
+console.log(address)

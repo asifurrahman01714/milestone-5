2c8bf0feb877382fa9530249b1a18f24bb512ebd
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 14:11:36 +0600

    Function without ES6

---
+++ Module 22/22-5 Arrow function, multiple parameter, function body.js
@@ -0,0 +1,18 @@
+// simple function
+function doubleIt1(x) {
+    return x*2;
+}
+const result1 = doubleIt1(5);
+console.log(result1);
+
+// function declaration as variable
+const doubleIt2 = function (x) {
+    return x*2;
+}
+console.log(doubleIt2(3));
+
+const doubleIt3 = function asif(x) {
+    return x*2;
+}
+console.log(doubleIt3(4));
+

861c40c095d2730b26e5a016cd35fc5d02a9372b
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 14:41:02 +0600

    Adding all array in previous system

---
+++ Module 22/22-6 Spread operator, concatenate multiple arrays, array max.js
@@ -0,0 +1,8 @@
+const array1 = [12,13,14,15]
+const array2 = [21,22,23,24];
+const array3 = [31,32,33,34];
+
+// Previous system
+const allArrays1 = array1.concat(array2).concat(5).concat(array3);
+console.log(allArrays1);
+

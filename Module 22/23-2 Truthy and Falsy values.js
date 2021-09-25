e4d8ce258969cf9bfe9c043f13a996fe77835d75
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 17:22:58 +0600

    Any number is true other than 0 and Any string is true other than empty string

---
+++ Module 22/23-2 Truthy and Falsy values.js
@@ -0,0 +1,15 @@
+// Any number is true other than 0
+let number1 = 0
+if (number1) {
+    console.log('true value')
+} else {
+    console.log('false value')
+}
+
+// Any string is true other than empty string
+let name ="0";
+if (name) {
+    console.log('true value')
+} else {
+    console.log('false value')
+}

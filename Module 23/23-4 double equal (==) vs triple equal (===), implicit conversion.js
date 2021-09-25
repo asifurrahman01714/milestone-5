bf1d08dbf01da8a05ddad7d3df9e24383d55667a
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-25 23:33:35 +0600

    0 is false and 1 is true

---
+++ Module 23/23-4 double equal (==) vs triple equal (===), implicit conversion.js
@@ -0,0 +1,15 @@
+const first = 0;
+const second =false;
+if (first == second) {
+    console.log('true')
+} else {
+    console.log('false')
+}
+
+const first1 =1;
+const second1 =true;
+if (first1 == second1) {
+    console.log('true')
+} else {
+    console.log('false')
+}

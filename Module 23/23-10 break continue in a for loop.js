ad0bc025a63a133382e8c8ba6418a1960047c949
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-29 18:43:18 +0600

    The task of continue is to skip some specific value of a loop

--- Module 23/23-10 break continue in a for loop.js
+++ Module 23/23-10 break continue in a for loop.js
@@ -10,4 +10,12 @@ for (let i = 0; i < nums.length; i++) {
     console.log(element);
 }
 
-
+// The task of continue is to skip some specific value of a loop
+const numbers = [1,-1, 2,-2, 3,-3, 4,-4, 5,-5, 6,-6, 7,-7, 8,-8];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element < 0) {
+        continue;
+    }
+    console.log(element)
+}

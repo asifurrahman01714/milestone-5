1cd8551bbece32b3ec85539ddeb8d3f155fbb0eb
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-09-29 17:52:52 +0600

    we can added another things in splicing

--- Module 23/23-9 Array slice, splice, array join elements.js
+++ Module 23/23-9 Array slice, splice, array join elements.js
@@ -7,8 +7,8 @@ console.log(nums);
 
 
 const names = ['asif', 'sakib', 'rakib', 'arif', 'hasib']
-const splicing = names.splice(0,2); // here 0 is the index number and 2 is the counting delete number
-
+// const splicing = names.splice(0,2); // here 0 is the index number and 2 is the counting delete number
+const splicing = names.splice(0,2, 'ahmad', 'bangladesh'); // we can added another things in splicing
 console.log(splicing);
 //The main array is  changed after splicing is applied on the array. It will remove the splicing number from the main array
 console.log(names)

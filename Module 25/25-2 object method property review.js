c66adc8328455b0974d20295df4c4fa4eec384f4
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-04 13:41:05 +0600

    we can call any value of an object key from a built in function or method  by this keyword.

--- Module 25/25-2 object method property review.js
+++ Module 25/25-2 object method property review.js
@@ -1,1 +1,17 @@
 // The function in an object is called a method
+const normalPerson = {
+    firstName: 'Asif Ur',
+    lastName: 'Rahman',
+    getFullName: function() { // this is called method
+        console.log(this.firstName + ' ' + this.lastName); // we can call any value of an object key from a built in function or method  by this keyword.
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
+console.log(normalPerson);
+console.log(normalPerson.firstName);
+console.log(normalPerson.lastName);
+console.log(normalPerson.getFullName());
+
+const result = normalPerson.getFullName();
+console.log(result);

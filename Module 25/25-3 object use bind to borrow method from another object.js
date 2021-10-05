094297449b28d88cafc7c03f80101a76906316f9
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-05 16:10:02 +0600

    The main purpose of bind methode

--- Module 25/25-3 object use bind to borrow method from another object.js
+++ Module 25/25-3 object use bind to borrow method from another object.js
@@ -1,1 +1,4 @@
 // Bind give a function as a return
+// The goal of bind method is to use a method of one object to another object.
+// In this case this bind method does not change the main object value.
+// Just like to use a Spade of another neighbor to create a hole in your home.

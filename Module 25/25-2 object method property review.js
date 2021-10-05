a08fd71f4a77949deec8b3f80f07ceb6c5ef9b72
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-05 16:21:11 +0600

    Bind method done

--- Module 25/25-2 object method property review.js
+++ Module 25/25-2 object method property review.js
@@ -2,13 +2,13 @@
 const normalPerson = {
     firstName: 'Asif Ur',
     lastName: 'Rahman',
-    getSalary: 12000,
+    salary: 12000,
     getFullName: function() { // this is called method
         console.log(this.firstName + ' ' + this.lastName); // we can call any value of an object key from a built in function or method  by this keyword.
         return this.firstName + ' ' + this.lastName;
     },
     chargeSalary: function (amount) {
-        return this.getSalary = this.getSalary - amount;
+        return this.salary = this.salary - amount;
     }
 
 }
@@ -24,4 +24,4 @@ console.log(result);
 // we can call the new salary by this given two way
 const salaryAfterCharge = normalPerson.chargeSalary(200);
 console.log(salaryAfterCharge);
-console.log(normalPerson.getSalary);
+console.log(normalPerson.salary);
--- Module 25/25-3 object use bind to borrow method from another object.js
+++ Module 25/25-3 object use bind to borrow method from another object.js
@@ -2,3 +2,39 @@
 // The goal of bind method is to use a method of one object to another object.
 // In this case this bind method does not change the main object value.
 // Just like to use a Spade of another neighbor to create a hole in your home.
+
+const normalPerson = {
+    firstName: 'Asif Ur',
+    lastName: 'Rahman',
+    salary: 12000,
+    getFullName: function() { // this is called method
+        console.log(this)
+        console.log(this.firstName + ' ' + this.lastName); // we can call any value of an object key from a built in function or method  by this keyword.
+        return this.firstName + ' ' + this.lastName;
+    },
+    chargeSalary: function (amount) {
+        //console.log(this)
+        return this.salary = this.salary - amount;
+    }
+
+}
+
+
+// result is same for this function call
+normalPerson.chargeSalary(300);
+normalPerson.getFullName();
+
+
+// bind will give you a function
+
+const heroPerson ={
+    name: 'Hero',
+    salary: 25000
+}
+
+// using chargeSalary method to this heroPerson
+const heroChargeSalary = normalPerson.chargeSalary.bind(heroPerson);
+// It will give a function named heroChargeSalary. So we have to call it to use.
+heroChargeSalary(300);
+console.log(heroPerson.salary)
+

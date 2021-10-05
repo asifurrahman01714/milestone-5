de951d15a800afffd452d70b308a8f52f28a0b14
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-05 15:45:12 +0600

    object method

--- Module 25/25-2 object method property review.js
+++ Module 25/25-2 object method property review.js
@@ -2,10 +2,15 @@
 const normalPerson = {
     firstName: 'Asif Ur',
     lastName: 'Rahman',
+    getSalary: 12000,
     getFullName: function() { // this is called method
         console.log(this.firstName + ' ' + this.lastName); // we can call any value of an object key from a built in function or method  by this keyword.
         return this.firstName + ' ' + this.lastName;
+    },
+    chargeSalary: function (amount) {
+        return this.getSalary = this.getSalary - amount;
     }
+
 }
 
 console.log(normalPerson);
@@ -15,3 +20,8 @@ console.log(normalPerson.getFullName());
 
 const result = normalPerson.getFullName();
 console.log(result);
+
+// we can call the new salary by this given two way
+const salaryAfterCharge = normalPerson.chargeSalary(200);
+console.log(salaryAfterCharge);
+console.log(normalPerson.getSalary);

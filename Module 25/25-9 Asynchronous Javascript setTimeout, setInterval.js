01d1ebaae8c3bcb0af6a31b70bb7ce3694010d83
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-09 14:31:43 +0600

    What is set timeout and setInterval

--- Module 25/25-9 Asynchronous Javascript setTimeout, setInterval.js
+++ Module 25/25-9 Asynchronous Javascript setTimeout, setInterval.js
@@ -1,3 +1,9 @@
+// Synchronous: means all the function will be executed with one after one.
+// Asynchronous: means all the function will not be executed one after one. The asynchronous function will be executed later.
+// We can also set a time to execute the function
+// Like: setTimeout and setInterval.
+// setTimeout: It will be executed for once just after the given time.
+// setInterval: It will be executed again and again between the time interval.
 console.log(2222)
 console.log(3333);
 function name() {

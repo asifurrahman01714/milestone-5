a5389bb14163ce8cb9a15d441de3e1947ae7411d
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-09 15:15:06 +0600

    time to exceute a function

---
+++ Module 25/25-11 JavaScript Datetime timezone and others.js
@@ -0,0 +1,9 @@
+// read moment js
+const start = new Date();
+let sum = 0;
+for (let i = 0; i < 1000000; i++) {
+    sum++;
+    // console.log(sum);
+}
+const end = new Date();
+console.log("Time elapsed: ", end - start);

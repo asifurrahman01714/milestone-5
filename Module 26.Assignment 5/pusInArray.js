a918ca4f6edb1b49287c7083198514b6a847cc08
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-12 21:20:07 +0600

    Facing problem with strIngredient

--- Module 26.Assignment 5/pusInArray.js
+++ Module 26.Assignment 5/pusInArray.js
@@ -1,6 +1,7 @@
 const ingredient = [];
 for (let i = 0; i < 20; i++) {
     const element = 'ingrediant'+ i;
-    ingredient.push(element)
+    ingredient.push(element);
+    
 }
 console.log(ingredient);

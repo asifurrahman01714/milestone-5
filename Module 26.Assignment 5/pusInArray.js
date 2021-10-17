d478dc0dfdfc87aa6ca6844b3bb76084bb6b76f1
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-17 21:23:42 +0600

    Exclude elements in one set that are not in another set in python

--- Module 26.Assignment 5/pusInArray.js
+++ Module 26.Assignment 5/pusInArray.js
@@ -5,3 +5,4 @@ for (let i = 0; i < 20; i++) {
     
 }
 console.log(ingredient);
+console.log(typeof ingredient);
---
+++ Python Cheat Sheet/practice.py
@@ -0,0 +1,4 @@
+A= {'apple', 'banana'} 
+B= {'cherry', 'apple'} 
+A.intersection_update(B) 
+print (A)

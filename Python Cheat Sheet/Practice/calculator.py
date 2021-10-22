713f974589a970d1042a7cdf88c9ebf69616673d
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-22 22:03:11 +0600

    Math function

---
+++ Python Cheat Sheet/Practice/calculator.py
@@ -0,0 +1,22 @@
+def add():
+    x = int(enterOne.get())
+    y = int(enterTwo.get())
+    result["text"] = x+y
+
+
+def sub():
+    x = int(enterOne.get())
+    y = int(enterTwo.get())
+    result["text"] = x-y
+
+
+def div():
+    x = int(enterOne.get())
+    y = int(enterTwo.get())
+    result["text"] = x/y
+
+
+def mul():
+    x = int(enterOne.get())
+    y = int(enterTwo.get())
+    result["text"] = x*y
---
+++ Python Cheat Sheet/Practice/math function.py
@@ -0,0 +1,12 @@
+import math 
+
+number1 = 4
+number2 = 2.5
+print(math.sqrt(number1))
+print(math.floor(number2))
+print(math.ceil(number2))
+print(math.factorial(number1))
+print(math.fabs(number2))
+print(math.log10(number1))
+print(math.sin(number1))
+print(math.pow(number1, number2))

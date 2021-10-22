a2b59755abdec352f1cd8c1c4e09801be16245c3
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-22 21:37:35 +0600

    Adding button to GUI

---
+++ Python Cheat Sheet/Practice/button to GUI.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+root.title("Programming Hero")
+course = Label(root, text="Course - Programming: Beginners To Advance")
+enroll = Button(root, text="Enroll Now", bd=5)
+course.pack()
+enroll.pack()
+root.mainloop()

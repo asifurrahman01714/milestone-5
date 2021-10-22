1f29f61774b48da7b644140e1292d57dd295f82a
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-22 14:09:01 +0600

    Try Except

---
+++ Python Cheat Sheet/Practice/try except.py
@@ -0,0 +1,6 @@
+try:
+    numerator = 4
+    denominator = 0 
+    print(numerator/denominator)
+except ZeroDivisionError: 
+    print("Cannot divide a number by zero, please try again with another valid value")

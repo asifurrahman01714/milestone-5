93649a16fc6b820f60e3cfceb48a11188d79625d
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-22 21:40:09 +0600

    Adding button color to GUI

---
+++ Python Cheat Sheet/Practice/button color.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+root.title("Programming Hero")
+course = Label(root, text="Course - Programming: Beginners To Advance")
+enroll = Button(root, text="Enroll Now", fg="blue", bg="yellow")
+course.pack()
+enroll.pack()
+root.mainloop()
+

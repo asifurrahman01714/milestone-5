3865eaa1f223771e25d1bf9734946a666e7f9195
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-22 14:10:58 +0600

    print error

--- Python Cheat Sheet/Practice/try except.py
+++ Python Cheat Sheet/Practice/try except.py
@@ -4,3 +4,11 @@ try:
     print(numerator/denominator)
 except ZeroDivisionError: 
     print("Cannot divide a number by zero, please try again with another valid value")
+
+# With error message
+try:
+    numerator = 4
+    denominator = 0 
+    print(numerator/denominator)
+except ZeroDivisionError as e: 
+    print("Cannot divide a number by zero, please try again with another valid value,", e)

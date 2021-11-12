29679b7fc3e5925875c639b498970ff271a94d85
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-11-12 23:05:09 +0600

    Hard rock music done

--- Module 26.5 Hard rock/app.js
+++ Module 26.5 Hard rock/app.js
@@ -20,19 +20,19 @@ const displaySongData = (data) => {
     const searchResults = document.getElementById('searchResults');
     for (let i = 0; i < data.length; i++) {
         const singleSong = data[i];
-        console.log(singleSong);
-        console.log(singleSong.title);
-        console.log(singleSong.artist.name);
+        const title = singleSong.title;
+        const artist = singleSong.artist.name;
+        console.log(title,artist);
         
         const resultDiv = document.createElement('div');
         resultDiv.innerHTML =`
         <div class="single-result row align-items-center my-3 p-3">
                     <div class="col-md-9" id="lyricsContainer">
-                        <h3 class="lyrics-name" id="lyricsName">${singleSong.title}</h3>
-                        <p class="author lead">Album by <span id="authorName">${singleSong.artist.name}</span></p>
+                        <h3 class="lyrics-name" id="lyricsName">${title}</h3>
+                        <p class="author lead">Album by <span id="authorName">${artist}</span></p>
                     </div>
                     <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success" onClick="getLyrics()">Get Lyrics</button>
+                        <button class="btn btn-success" onClick="getLyrics('${title}','${artist}')">Get Lyrics</button>
                     </div>
             </div>
         `;
@@ -40,6 +40,22 @@ const displaySongData = (data) => {
     }
  }
 
- const getLyrics = () => {
-     console.log('lyrics collected');
+ const getLyrics = (title, artist) => {
+     console.log(title, artist);
+     const url=`https://api.lyrics.ovh/v1/${artist}/${title}`;
+        fetch(url)
+        .then(response => response.json())
+        .then(data => displayLyrics(data))
+ }
+
+ const displayLyrics = (data) => {
+     console.log(data.lyrics);
+     const lyricsContainer = document.getElementById('lyricsContainer');
+     let lyricsContent = document.getElementById('lyricsContent');
+     lyricsContent = data.lyrics;
+     if (lyricsContent === undefined) {
+         lyricsContainer.innerHTML = '<h2>No Lyrics Found</h2>'
+     } else {
+         lyricsContainer.innerHTML = lyricsContent;
+     }
  }
